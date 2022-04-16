ac9131d6994cfb602c221654ff386ba2f42412dd
lukasz_kusyk_psw <lukifrancuz@gmail.com>
2022-04-16 14:04:04 +0200

    add camera manager script

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CameraManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        PostProcessVolume postProcess = GetComponent<PostProcessVolume>();
+
+        if (postProcess == null)
+            return;
+
+        //postProcess.enabled = SettingsManager.settings.graphics == 0 ? postProcess.enabled = false : postProcess.enabled = true;
+    }
+}
