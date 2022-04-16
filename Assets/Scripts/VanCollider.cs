e867ac1ea0f708bde2e764e60da7bb8977220b70
lukasz_kusyk_psw <lukifrancuz@gmail.com>
2022-04-16 14:05:24 +0200

    add vancollider.cs

---
+++ Assets/Scripts/VanCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanCollider : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerController.Instance.Lose();
+    }
+}
