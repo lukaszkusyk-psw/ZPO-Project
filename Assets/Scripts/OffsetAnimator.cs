22e2a754f9d15e9059787fbaf7fe22ee2d988acb
lukasz_kusyk_psw <lukifrancuz@gmail.com>
2022-04-16 14:05:12 +0200

    add scrolling textures on road and van

---
+++ Assets/Scripts/OffsetAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetAnimator : MonoBehaviour
+{
+    public Vector2 offset = new Vector2();
+    public float speed = 0.5f;
+
+    private Material material;
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        material.mainTextureOffset += offset * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/TextureScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroller : MonoBehaviour
+{
+    private Material material;
+    public float multiplier = 1f;
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        material.mainTextureOffset = new Vector2(material.mainTextureOffset.x, 
+            material.mainTextureOffset.y - PlayerController.CurrentSpeed * Time.deltaTime * multiplier * PlayerController.MinigameSpeed);
+    }
+}
